6e2980802494024054a7ab99424510685364a632
toshiki tomihira <toshiki19970714@gmail.com>
2021-07-31 18:35:02 +0900

    Format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,80 @@
-import { useRef, useEffect } from "react";
-import "@tensorflow/tfjs-core";
-import "@tensorflow/tfjs-converter";
-import "@tensorflow/tfjs-backend-webgl";
-import Webcam from "react-webcam";
-import { createDetector, PoseDetector, SupportedModels, InputResolution } from "@tensorflow-models/pose-detection";
-import { Rendering } from "./models/rendering";
+import { useRef, useEffect } from 'react'
+import '@tensorflow/tfjs-core'
+import '@tensorflow/tfjs-converter'
+import '@tensorflow/tfjs-backend-webgl'
+import Webcam from 'react-webcam'
+import {
+  createDetector,
+  PoseDetector,
+  SupportedModels,
+  InputResolution,
+} from '@tensorflow-models/pose-detection'
+import { Rendering } from './models/rendering'
 // import {getValidInputResolutionDimensions, getValidOutputResolutionDimensions} from "./utils";
 
 export default function App() {
-  const webcam = useRef<Webcam>(null);
-  const canvas = useRef<HTMLCanvasElement>(null);
-  const modelName = SupportedModels.PoseNet;
+  const webcam = useRef<Webcam>(null)
+  const canvas = useRef<HTMLCanvasElement>(null)
+  const modelName = SupportedModels.PoseNet
 
   const videoConstraints = {
     width: 640,
     height: 480,
-    facingMode: "user"
-  };
-  
+    facingMode: 'user',
+  }
+
   const runPoseDetect = async () => {
-    const resolution: InputResolution = { width: 500, height: 500 };
+    const resolution: InputResolution = { width: 500, height: 500 }
     const detector = await createDetector(modelName, {
       quantBytes: 4,
       architecture: 'MobileNetV1',
       outputStride: 16,
       inputResolution: resolution,
-      multiplier: 0.75
-    });
-    detect(detector);
-  };
+      multiplier: 0.75,
+    })
+    detect(detector)
+  }
 
   const detect = async (detector: PoseDetector) => {
     if (webcam.current && canvas.current) {
-      const webcamCurrent = webcam.current as any;
+      const webcamCurrent = webcam.current as any
       // go next step only when the video is completely uploaded.
       if (webcamCurrent.video.readyState === 4) {
-        const video = webcamCurrent.video;
-        const videoWidth = webcamCurrent.video.videoWidth;
-        const videoHeight = webcamCurrent.video.videoHeight;
-        video.width = videoWidth;
-        video.height = videoHeight;
+        const video = webcamCurrent.video
+        const videoWidth = webcamCurrent.video.videoWidth
+        const videoHeight = webcamCurrent.video.videoHeight
+        video.width = videoWidth
+        video.height = videoHeight
 
-        canvas.current.width = videoWidth;
-        canvas.current.height = videoHeight;
-        console.log(`videoWidth: ${videoWidth}, videoHeight: ${videoHeight}`);
+        canvas.current.width = videoWidth
+        canvas.current.height = videoHeight
+        console.log(`videoWidth: ${videoWidth}, videoHeight: ${videoHeight}`)
 
-        const predictions = await detector.estimatePoses(
-          video,
-          {maxPoses: 1, flipHorizontal: false}
-        );
+        const predictions = await detector.estimatePoses(video, {
+          maxPoses: 1,
+          flipHorizontal: false,
+        })
         if (predictions.length) {
-          console.log(predictions);
+          console.log(predictions)
         }
 
-        const ctx = canvas.current.getContext("2d") as CanvasRenderingContext2D;
-        const rendering = new Rendering(modelName, ctx);
+        const ctx = canvas.current.getContext('2d') as CanvasRenderingContext2D
+        const rendering = new Rendering(modelName, ctx)
         requestAnimationFrame(() => {
-          rendering.drawResult(predictions[0]);
-        });
-        await detect(detector);
+          rendering.drawResult(predictions[0])
+        })
+        await detect(detector)
       } else {
         setTimeout(() => {
-          detect(detector);
-        }, 100);
-      };
-    };
-  };
+          detect(detector)
+        }, 100)
+      }
+    }
+  }
 
   useEffect(() => {
-    runPoseDetect();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    runPoseDetect()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -82,9 +87,9 @@ export default function App() {
         videoConstraints={videoConstraints}
         ref={webcam}
         style={{
-          position: "absolute",
-          margin: "auto",
-          textAlign: "center",
+          position: 'absolute',
+          margin: 'auto',
+          textAlign: 'center',
           top: 100,
           left: 0,
           right: 0,
@@ -94,9 +99,9 @@ export default function App() {
       <canvas
         ref={canvas}
         style={{
-          position: "absolute",
-          margin: "auto",
-          textAlign: "center",
+          position: 'absolute',
+          margin: 'auto',
+          textAlign: 'center',
           top: 100,
           left: 0,
           right: 0,
--- pages/models/rendering.ts
+++ pages/models/rendering.ts
@@ -1,29 +1,29 @@
 // ref: https://github.com/tensorflow/tfjs-models/blob/master/pose-detection/demos/live_video/src/camera.js
 
-import * as posedetection from "@tensorflow-models/pose-detection";
+import * as posedetection from '@tensorflow-models/pose-detection'
 import {
   SupportedModels,
   Keypoint,
   Pose,
-} from "@tensorflow-models/pose-detection";
+} from '@tensorflow-models/pose-detection'
 
 export const POSENET_CONFIG = {
   maxPoses: 1,
   scoreThreshold: 0.5,
-};
+}
 
-export const DEFAULT_LINE_WIDTH = 2;
-export const DEFAULT_RADIUS = 4;
+export const DEFAULT_LINE_WIDTH = 2
+export const DEFAULT_RADIUS = 4
 
 export class Rendering {
-  ctx: CanvasRenderingContext2D;
-  modelName: SupportedModels;
-  modelConfig: any;
+  ctx: CanvasRenderingContext2D
+  modelName: SupportedModels
+  modelConfig: any
 
   constructor(model: SupportedModels, context: CanvasRenderingContext2D) {
-    this.modelName = model;
-    this.ctx = context;
-    this.modelConfig = { ...POSENET_CONFIG };
+    this.modelName = model
+    this.ctx = context
+    this.modelConfig = { ...POSENET_CONFIG }
   }
 
   /**
@@ -32,8 +32,8 @@ export class Rendering {
    */
   drawResult(pose: Pose) {
     if (pose.keypoints != null) {
-      this.drawKeypoints(pose.keypoints);
-      this.drawSkeleton(pose.keypoints);
+      this.drawKeypoints(pose.keypoints)
+      this.drawSkeleton(pose.keypoints)
     }
   }
   /**
@@ -43,36 +43,36 @@ export class Rendering {
   drawKeypoints(keypoints: Keypoint[]) {
     const keypointInd = posedetection.util.getKeypointIndexBySide(
       this.modelName
-    );
-    this.ctx.fillStyle = "White";
-    this.ctx.strokeStyle = "White";
-    this.ctx.lineWidth = DEFAULT_LINE_WIDTH;
+    )
+    this.ctx.fillStyle = 'White'
+    this.ctx.strokeStyle = 'White'
+    this.ctx.lineWidth = DEFAULT_LINE_WIDTH
 
     for (const i of keypointInd.middle) {
-      this.drawKeypoint(keypoints[i]);
+      this.drawKeypoint(keypoints[i])
     }
 
-    this.ctx.fillStyle = "Green";
+    this.ctx.fillStyle = 'Green'
     for (const i of keypointInd.left) {
-      this.drawKeypoint(keypoints[i]);
+      this.drawKeypoint(keypoints[i])
     }
 
-    this.ctx.fillStyle = "Orange";
+    this.ctx.fillStyle = 'Orange'
     for (const i of keypointInd.right) {
-      this.drawKeypoint(keypoints[i]);
+      this.drawKeypoint(keypoints[i])
     }
   }
 
   drawKeypoint(keypoint: Keypoint) {
     // If score is null, just show the keypoint.
-    const score = keypoint.score != null ? keypoint.score : 1;
-    const scoreThreshold = this.modelConfig.scoreThreshold || 0;
+    const score = keypoint.score != null ? keypoint.score : 1
+    const scoreThreshold = this.modelConfig.scoreThreshold || 0
 
     if (score >= scoreThreshold) {
-      const circle = new Path2D();
-      circle.arc(keypoint.x, keypoint.y, DEFAULT_RADIUS, 0, 2 * Math.PI);
-      this.ctx.fill(circle);
-      this.ctx.stroke(circle);
+      const circle = new Path2D()
+      circle.arc(keypoint.x, keypoint.y, DEFAULT_RADIUS, 0, 2 * Math.PI)
+      this.ctx.fill(circle)
+      this.ctx.stroke(circle)
     }
   }
 
@@ -81,25 +81,25 @@ export class Rendering {
    * @param keypoints A list of keypoints.
    */
   drawSkeleton(keypoints: Keypoint[]) {
-    this.ctx.fillStyle = "White";
-    this.ctx.strokeStyle = "White";
-    this.ctx.lineWidth = DEFAULT_LINE_WIDTH;
+    this.ctx.fillStyle = 'White'
+    this.ctx.strokeStyle = 'White'
+    this.ctx.lineWidth = DEFAULT_LINE_WIDTH
 
     posedetection.util.getAdjacentPairs(this.modelName).forEach(([i, j]) => {
-      const kp1 = keypoints[i];
-      const kp2 = keypoints[j];
+      const kp1 = keypoints[i]
+      const kp2 = keypoints[j]
 
       // If score is null, just show the keypoint.
-      const score1 = kp1.score != null ? kp1.score : 1;
-      const score2 = kp2.score != null ? kp2.score : 1;
-      const scoreThreshold = this.modelConfig.scoreThreshold || 0;
+      const score1 = kp1.score != null ? kp1.score : 1
+      const score2 = kp2.score != null ? kp2.score : 1
+      const scoreThreshold = this.modelConfig.scoreThreshold || 0
 
       if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(kp1.x, kp1.y);
-        this.ctx.lineTo(kp2.x, kp2.y);
-        this.ctx.stroke();
+        this.ctx.beginPath()
+        this.ctx.moveTo(kp1.x, kp1.y)
+        this.ctx.lineTo(kp2.x, kp2.y)
+        this.ctx.stroke()
       }
-    });
+    })
   }
 }
