775e80a6e77815c068b1ba11cd40765b88d66fdd
yuikoito <yuiko.dev@gmail.com>
2021-08-01 23:22:10 +0900

    chore::bug: styled-componentsを使わない && ダウンロードリンクの作成

--- src/components/RecordedVideo.tsx
+++ src/components/RecordedVideo.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { isSafari } from 'react-device-detect'
+import { DefaultButton, PinkButton } from '../components/Buttons'
 
 export const RecordedVideo = (props: { recordedChunks: BlobPart[] }) => {
   const [videoUrl, setVideoUrl] = useState<string>('')
@@ -25,18 +26,54 @@ export const RecordedVideo = (props: { recordedChunks: BlobPart[] }) => {
     }
   }
 
+  const saveVideo = () => {
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    document.body.appendChild(a)
+    a.download = url
+    a.href = url
+    a.click()
+    a.remove()
+    URL.revokeObjectURL(url)
+  }
+
   return (
-    <>
+    // styled-componentsで書くと動画再生ができないので、一旦styleベタ書きにしてる
+    <div
+      style={{
+        position: 'absolute',
+        margin: 'auto',
+        textAlign: 'center',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 9,
+        background: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <h2
+        style={{
+          textAlign: 'center',
+          fontWeight: 700,
+          fontSize: '25px',
+          color: '#0a2569',
+          margin: 0,
+        }}
+      >
+        Pictogram-san
+        <br />
+        Challenge
+      </h2>
       <video
         ref={videoRef}
         style={{
-          position: 'absolute',
-          margin: 'auto',
-          textAlign: 'center',
-          top: 0,
-          left: 0,
-          right: 0,
-          zIndex: 9,
+          maxWidth: '250px',
         }}
         controls
         src={videoUrl}
@@ -49,15 +86,18 @@ export const RecordedVideo = (props: { recordedChunks: BlobPart[] }) => {
           style={{
             position: 'absolute',
             margin: 'auto',
-            textAlign: 'center',
             top: 0,
             bottom: 0,
             left: 0,
             right: 0,
-            zIndex: 9,
+            cursor: 'pointer',
           }}
         />
       )}
-    </>
+      <div style={{ display: 'flex', gap: '5px', marginTop: '10px' }}>
+        <PinkButton>Try again</PinkButton>
+        <DefaultButton onClick={saveVideo}>Save Video</DefaultButton>
+      </div>
+    </div>
   )
 }
